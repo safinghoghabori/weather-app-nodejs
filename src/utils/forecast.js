72555df0835bb34c61002b3bd8744395a65c157e
unknown <safinghoghabori65@gmail.com>
2020-06-06 16:29:07 -0700

    added feature in weather information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,6 @@ const request = require("request");
 
 const forecast = (lat, log, callback) => {
   const urlWeather = `http://api.weatherapi.com/v1/current.json?key=b562174b5a7c4cd4a16125405203005&q=${lat},${log}`;
-
   request({ url: urlWeather, json: true }, (error, response) => {
     if (error) {
       callback("Unable to connect the location service!", undefined);
@@ -10,6 +9,7 @@ const forecast = (lat, log, callback) => {
       callback(undefined, {
         temprature: response.body.current.temp_c,
         cloud: response.body.current.cloud,
+        localtime: response.body.location.localtime,
       });
     }
   });
